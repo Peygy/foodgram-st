7063e156ef207d2921b957f7f2aa2203ed2a630b
Leonid Isakov <isakov.29072004@gmail.com>
2025-05-28 00:38:51 +0300

    fix validator

---
+++ backend/users/validators.py
@@ -0,0 +1,14 @@
+import re
+
+from rest_framework.validators import ValidationError
+
+
+def validator_username(value):
+    """Проверка имени и возврат не корректных символов."""
+    bad_chars = "".join(re.split(r"^[\w.@+-]+\Z", value))
+
+    if len(bad_chars) != 0:
+        raise ValidationError(
+            f"Есть недопустимые символы: {bad_chars}"
+            f"Для поля `username` не должны приниматься значения $"
+        )
